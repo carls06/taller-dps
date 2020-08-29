9bea76815819dc1ceeb86fba6dc8e4c0d3263d6a
Carlos Gonzalez <contactos503@gmail.com>
2020-08-28 22:58:39 -0600

    .

--- src/app/services/taller.service.ts
+++ src/app/services/taller.service.ts
@@ -1,19 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Taller } from '../models/cliente.model';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class TallerService {
+    clientes: Taller[] = [];
+    constructor() {
+        this.cargarStorage();
+    }
 
-  constructor() { }
+    guardarStorage() {
+        localStorage.setItem('data', JSON.stringify(this.clientes));
+    }
+
+    crearCliente(dui:string){
+        const nuevaLista=new Taller(dui);
+        this.clientes.push(nuevaLista);
+        this.guardarStorage();
+        return nuevaLista.dui;
+        
+    
+       }
+
+    cargarStorage() {
+        if (localStorage.getItem('data')) {
+            this.clientes = JSON.parse(localStorage.getItem('data'));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
-
-export interface taller{
-    nombre:string;
-    dui:string;
-    vehiculo:string;
-    costo:number;
-
-}
